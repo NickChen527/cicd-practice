595732697d65450d04ab52fb89ccf52825ac5685
SL-Nick-Chen <nick.chen@softleader.com.tw>
2026-03-18 11:05:40 +0800

    init

---
+++ ai-agent/src/test/java/com/nick/ai_agent/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nick.ai_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
